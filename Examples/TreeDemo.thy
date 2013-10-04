51097773aac3acfb1e8f3b2e35b90d3b2f1d67a5
Moa Johansson <moa.johansson@chalmers.se>
2013-10-04 14:39:39 +0200

    ready to pass conjectures to HipSpec.

--- Examples/TreeDemo.thy
+++ Examples/TreeDemo.thy
@@ -19,42 +19,13 @@ where
 | "map f (Node l r) = Node (map f l) (map f r)" 
 
 
-
-ML{*
-
-val consts = ["TreeDemo.mirror","TreeDemo.tmap"];
-
-
-val s = Context.theory_name @{theory}
-*}
-
-
 lemma "map_mirror": "map a (mirror a1) = mirror (map a a1)"
-    apply (tactic {* fn thm => let val _ = (Trm.MLSerialise.print_term (Thm.concl_of thm)) in Seq.single thm end;  *})
-
   apply (tactic {* HipSpec.hipspec *})
 apply (induct a1, simp_all)
 done
 
 
-(*by (induct a1, simp_all) *)
-ML{* 
 
-val thm =  @{thm "map_mirror"};
-val t = Thm.concl_of thm;
-val renamings = Symtab.make (HipSpec.compute_renamings @{theory} ["TreeDemo.map","TreeDemo.mirror"]);
-
-*}
-ML{*
-
-Symtab.lookup renamings "TreeDemo.map";
-replace_renamings renamings "TreeDemo.map";
-
-(*
-val s = HipSpec.hipspec_prop_of_thm @{thm "map_mirror"};
-writeln s; 
-*)
-*}
 
 
 ML{*
