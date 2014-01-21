b1d131bf31bc341332b969f038f5443037f3caf2
Moa Johansson <moa.johansson@chalmers.se>
2014-01-21 17:15:12 +0100

    Implemented a HipSpec style loop for Isabelle which applies structural induction

--- Examples/Dev.thy
+++ Examples/Dev.thy
@@ -1,14 +1,25 @@
 theory Dev
-imports Main
+imports HOL_IsaP
 uses "../ProofTools.ML"
 
 begin
 (* uses "../HipSpec.ML"
 *)
 
+
 datatype 'a Tree = 
   Leaf 'a 
   | Node "'a Tree""'a Tree"
+ML{*
+InductDTac.induct_tac;
+InductDTac.inductable_things_in_term;
+val trm = @{term "x :: 'a Tree"};
+
+val vs = ProofTools.inductable_things_in_term @{theory} trm;
+
+val (x,ty) = Term.dest_Free trm;
+val s = x ^ " :: ";
+*}
 
 definition unit :: "'a => 'a Tree"
 where
@@ -33,18 +44,37 @@ where
   "toList (Leaf a) = [a]"
   | "toList (Node l r) = (toList l) @ (toList r)"
 
-
 ML{*
-(*val c = ProofTools.mk_conjs @{context} ["mirror (unit x) = Leaf x"];
+val thy = @{theory};
+val ctxt = @{context};
+val trm = @{term "mirror(mirror x) = x"};
+
+val conj = (Goal.init o (Thm.cterm_of thy) o (Syntax.read_prop ctxt)) "mirror(mirror x) = x";
+val conj2 = (Goal.init o (Thm.cterm_of thy) o (Syntax.read_prop ctxt)) "x @ y = y @ x";
+val vars = map fst (fst (ProofTools.inductable_things_in_sg 1 conj2));
+InductDTac.induct_tac NONE [hd vars] conj2
+(*
+val s1 = Seq.list_of((InductDTac.induct_tac NONE vars conj2));
+
+val s1 = Seq.list_of(Seq.maps (ProofTools.prove @{simpset}) (InductDTac.induct_tac NONE vars conj));
+
+val t1 = (Induct_Tacs.induct_tac ctxt [[SOME "x :: 'a Tree"]] 1 conj);
+val [t2] = Seq.list_of(Seq.maps (ProofTools.prove @{simpset}) t1);
+Goal.finish @{context} t2;
 *)
-val t = Syntax.read_term  @{context} "Trueprop([x] = toList(Leaf x))";
-val ct = Thm.cterm_of (Proof_Context.theory_of @{context}) t;
-val thm = Goal.init ct;
-
-val x = Seq.hd(ProofTools.simp_all (HOL_basic_ss addsimps @{thms "toList.simps"}) thm);
-
 *}
 
+
+ML{*
+val c = ProofTools.mk_conjs @{context} ["[x] = toList(Leaf x)", "[] = toList(Leaf x)"];
+val x  = ProofTools.hipspec_loop c;
+*}
+
+ML{*
+val (ProofTools.ConjQ x) = the x;
+val [thm] = (#proved x);
+Goal.finish @{context} thm;
+*}
 ML{*
 val filepath = "~/TheoremProvers/IsaHip/";
 val hipspecifyer_cmd = filepath^"HipSpecifyer";
