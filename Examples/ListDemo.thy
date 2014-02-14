c58c0d72ed2598afbaac6d0a991f74697dd2e611
moajohansson <moa.johansson@chalmers.se>
2014-02-14 20:09:12 +0000

    Refactored the code, now functorised by hard and routine tactics, adn uses Isabelle contexts properly during the exploration loop

--- Examples/ListDemo.thy
+++ Examples/ListDemo.thy
@@ -27,30 +27,56 @@ where
   "qrev Emp a = a"
 | "qrev (Cons x xs) a = qrev xs (Cons x a)"
 
-ML{*HipSpec.explore @{context} ["ListDemo.app"];
+ML{*Hipster_Explore.explore @{context} ["ListDemo.app"];
  *}
-lemma lemma_a [thy_expl] : "app x2 Emp = x2"
-by (tactic {* ProofTools.induct_and_metis @{context} (@{thms ListDemo.app.simps} @ []) *})
+lemma lemma_a [thy_expl]: "app x2 Emp = x2"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.app.simps thy_expl} *})
 
-lemma lemma_b [thy_expl] : "app (app x2 y2) z2 = app x2 (app y2 z2)"
-by (tactic {* ProofTools.induct_and_metis @{context} (@{thms ListDemo.app.simps} @ [@{thm lemma_a}]) *})
-
-ML{* val [t1,t2] = @{thms thy_expl};
-Thm.get_tags t1;
-Long_Name.base_name(Thm.get_name_hint t1); (* This is prefixed by theory *)
-*}
-
-ML {* val consts = ["ListDemo.app", "ListDemo.rev" ] ; *}
+lemma lemma_aa [thy_expl]: "app (app x2 y2) z2 = app x2 (app y2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.app.simps thy_expl} *})
 
 ML{*
-HipSpec.explore @{context} consts;
-
+Hipster_Explore.explore @{context}  ["ListDemo.app", "ListDemo.rev"];
 *}
-lemma lemma_c [thy_expl] : "app (ListDemo.rev x5) (ListDemo.rev y5) = ListDemo.rev (app y5 x5)"
-by (tactic {* ProofTools.induct_and_metis @{context} (@{thms ListDemo.app.simps} @ @{thms ListDemo.rev.simps} @ []) *})
+lemma lemma_ab [thy_expl]: "app (ListDemo.rev x5) (ListDemo.rev y5) = ListDemo.rev (app y5 x5)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.app.simps ListDemo.rev.simps thy_expl} *})
 
-lemma lemma_d [thy_expl] : "ListDemo.rev (ListDemo.rev x5) = x5"
-by (tactic {* ProofTools.induct_and_metis @{context} (@{thms ListDemo.app.simps} @ @{thms ListDemo.rev.simps} @ [@{thm lemma_c}]) *})
+lemma lemma_ac [thy_expl]: "ListDemo.rev (ListDemo.rev x5) = x5"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.app.simps ListDemo.rev.simps thy_expl} *})
+
+ML{* Hipster_Explore.explore @{context} ["ListDemo.qrev", "ListDemo.rev"]; *}
+lemma lemma_ad [thy_expl]: "app (qrev x2 y2) z2 = qrev x2 (app y2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.qrev.simps ListDemo.rev.simps thy_expl} *})
+
+lemma lemma_ae [thy_expl]: "qrev (app x2 y2) z2 = qrev y2 (qrev x2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.qrev.simps ListDemo.rev.simps thy_expl} *})
+
+lemma lemma_af [thy_expl]: "qrev (qrev x2 y2) z2 = qrev y2 (app x2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.qrev.simps ListDemo.rev.simps thy_expl} *})
+
+lemma lemma_ag [thy_expl]: "qrev x5 (ListDemo.rev y5) = ListDemo.rev (app y5 x5)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.qrev.simps ListDemo.rev.simps thy_expl} *})
+
+
+ML{* Hipster_Explore.explore @{context} ["ListDemo.app","ListDemo.qrev", "ListDemo.rev"]; *}
+(* At this point HipSpec is done! *)
+
+lemma f
+ explore
+proof -
+  have lemma1: f
+    proof
+  have lemma2: g
+    proof
+  from lemma1 lemma2 show ?thesis
+    proof
+qed
+
+ML{*
+open Active;
+Active.sendback_markup;
+*}
+
 
 ML{*
 HipsterRules.get @{context};
@@ -60,115 +86,6 @@ HipsterRules.get @{context};
       Active.sendback_markup [Markup.padding_command] "by (metis rev.simps(2))" ^ ".")
 *)
    *}
-
-
-
-lemma "rev xs = qrev xs Emp"
-apply (metis hipster_thms app.simps rev.simps qrev.simps)
-done
-
-ML{*
-val ctxt = @{context};
-val facts = maps (fn c => Sledgehammer_Util.thms_of_name ctxt (c^".simps")) consts
-val (lems0, ctxt', fails) = HipSpec.hipspec_explore ctxt facts consts;
-
-*}
-ML{*
-val [l1,l2,l3] = lems0;
-val lems = [Thm.put_name_hint "hipspec1" l1, 
-  Thm.put_name_hint "hipspec2" l2, Thm.put_name_hint "hipspec3" l3];
-*}
-ML{* 
-Context.Proof;
-Proof_Context.export ctxt' ctxt lems; *}
-
-
-
-lemma "rev xs = qrev xs Emp"
-apply (tactic {* ProofTools.timed_metis_tac @{context} @{thms hipster_thms} 1 *})
-done
-
-
-ML {* 
-
-val (thms,s, fails) = HipSpec.hipspec_explore @{theory} consts;
-
-map Pretty.writeln (map ((Syntax.pretty_term @{context}) o prop_of) thms);
-*}
-lemma "rev(rev xs) = xs"
-apply (tactic {* ProofTools.timed_metis_tac @{context} (@{thms rev.simps} @ @{thms app.simps} @ thms) 1 *})
-done
-
-lemma "app xs (app ys zs) = app (app xs ys) zs"
-apply (tactic {* ProofTools.timed_metis_tac @{context} (@{thms rev.simps} @ @{thms app.simps} @ thms) 1 *})
-done
-
-lemma "rev xs = qrev xs Emp"
-apply (tactic {* ProofTools.timed_metis_tac @{context} (@{thms rev.simps} @ @{thms app.simps} @ thms) 1 *})
-done
-
-
-ML{*
-
-Pretty.str_of;
-map Pretty.writeln (map ((Syntax.pretty_term @{context}) o prop_of) thms);
-
-*}
-ML{*
-(* map (Sledgehammer_Util.thms_of_name @{context}) consts; *)
-
-*}
-lemma "rev(rev xs) = xs"
-apply (induct xs)
-apply simp
-apply simp
-apply (tactic {* mymetis @{context} (@{thms rev.simps} @ @{thms app.simps} @ thms) 1 *})
-
-
-lemma app_nil[simp]: "app xs Emp = xs"
-apply (tactic {* timed_metis_tac @{context} [@{thm ListDemo.rev.simps(2)}, @{thm app.simps(2)}] 1 *})
-apply (induct xs)
-apply simp_all
-done
-
-lemma app_assoc[simp]: "app (app xs ys) zs = app xs (app ys zs)"
-apply (induct xs)
-apply simp_all
-done
-ML{*
-
-*}
-lemma rev_app_2: "app (rev ys) (rev xs) =  rev (app xs ys)"
-apply (induct xs)
-apply simp
-apply (tactic {* mymetis @{context} [@{thm ListDemo.rev.simps(2)}, @{thm app.simps(2)}, @{thm app_assoc}] 1*} )
- (* apply (metis rev.simps app.simps app_assoc)
-apply (drule sym)
-apply simp *)
-done
-
-lemma "rev(rev xs) = xs"
-apply (induct xs)
-apply simp
-apply (simp add:rev_app_2)
-apply (metis rev.simps app.simps app_assoc rev_app_2)
-
-
-
-theorem "mythm" : "app (app x y) z = app x (app y z)"
-apply (rule sym)
-apply (tactic  {*ProofTools.prove_by_simp s*})
-done
-lemma "app (app x y) x = app x (app y x)"
-by (simp add : mythm)
-
-lemma "rev xs = qrev xs Emp"
-apply (induct xs)
-apply simp
-apply simp
-(*apply (tactic {* (HipSpec.hipspec_explore_tac consts) *}) *)
-sorry
-
 
 
 end
--- IsaHipster.thy
+++ IsaHipster.thy
@@ -3,32 +3,37 @@ imports Main
 begin
 
 ML{*
-
-structure HipsterNames = 
+structure Hipster_Setup =
 struct
 
-val name_index = Attrib.setup_config_int @{binding "hipster_name_ind"} (K 1)
- 
-fun new_name thy = 
-    let 
-    val i = Config.get_global thy name_index
-    val thy' = Config.put_global name_index (i+1) thy
-    in  (thy',"hipster_lemma_"^ (Library.string_of_int i)) end;
+(* FIXME: Default to Isabelle Contrib or something more sensible *)
+val basepath = "~/TheoremProvers/IsaHipster/";
+val filepath = basepath^"GenCode/";
 
-end;
+(* 'hipspecifyer_cmd' is the path to the HipSpecifyer executable,        *)
+(*  which post-process the Haskell file so HipSpec gets generators etc.  *)
+
+val hipspecifyer_cmd = basepath^"HipSpecifyer";
+
+end
 *}
 
-ML{*
-structure HipsterRules = Named_Thms
+ML {*
+structure Hipster_Rules = Named_Thms
 (val name = @{binding "thy_expl"} 
  val description = "Theorems discovered by theory exploration")
 *}
-setup {* HipsterRules.setup;*}
 
+setup {* Hipster_Rules.setup;*}
 
-
+ML_file "HipsterUtils.ML"
+ML_file "ThyExplData.ML"
+ML_file "HipsterTacs.ML"
+ML_file "ThyExplTacs.ML"
+ML_file "HipsterExplore.ML"
+(*
 ML_file "ProofTools.ML"
 ML_file "HipSpec.ML"
-
+*)
 
 end
