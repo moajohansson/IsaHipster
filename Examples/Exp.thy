b43bfe35575f4ca7b641baecc157884c5723c508
moajohansson <moa.johansson@chalmers.se>
2014-02-26 14:33:01 +0100

    Fixed Expression compiler example

--- Examples/Exp.thy
+++ Examples/Exp.thy
@@ -7,51 +7,72 @@ begin
 higher-order datatypes like expr *)
 (* type_synonym 'v binop = "'v \<Rightarrow> 'v \<Rightarrow> 'v" *)
 
-datatype ('a, 'v, 'b) expr =
-  Cex 'v |
-  Vex 'a |
-  Bex "'b" "('a,'v,'b) expr" "('a,'v,'b) expr"
+datatype ('c, 'v, 'b) expr =
+  Cex 'c |
+  Vex 'v |
+  Bex "'b" "('c,'v,'b) expr" "('c,'v,'b) expr"
 
-primrec "value" :: "('b \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> 'v) \<Rightarrow> ('a,'v,'b) expr \<Rightarrow> ('a \<Rightarrow> 'v) \<Rightarrow> 'v"
+fun "value" :: "('b \<Rightarrow> 'c \<Rightarrow> 'c \<Rightarrow> 'c) \<Rightarrow> ('v \<Rightarrow> 'c) \<Rightarrow> ('c,'v,'b) expr \<Rightarrow> 'c"
 where
-  "value i (Cex v) env = v" |
-  "value i (Vex a) env = env a" |
-  "value i (Bex f e1 e2) env = (i f) (value i e1 env) (value i e2 env)"
+  "value getBinop env (Cex c) = c" |
+  "value getBinop env (Vex v) = env v" |
+  "value getBinop env (Bex b e1 e2) = (getBinop b) (value getBinop env e1) (value getBinop env e2)"
 
-datatype ('a, 'v, 'b) instr =
-  Const 'v
-  | Load 'a
-  | Apply "'b"
+datatype ('c, 'v, 'b) program =
+  Done
+  | Const 'c "('c, 'v, 'b) program"
+  | Load 'v "('c, 'v, 'b) program"
+  | Apply 'b "('c, 'v, 'b) program"
 
-datatype ('a, 'v, 'b) instr_list =
-  Instr_nil
-  | Instr_cons "('a, 'v, 'b) instr" "('a, 'v, 'b) instr_list"
-
-primrec instr_app :: "('a, 'v, 'b) instr_list => ('a, 'v, 'b) instr_list => ('a, 'v, 'b) instr_list"
+fun sequence :: "('c, 'v, 'b) program => ('c, 'v, 'b) program => ('c, 'v, 'b) program"
 where
-  "instr_app Instr_nil xs = xs"
-| "instr_app (Instr_cons x xs) ys = Instr_cons x (instr_app xs ys)"
+  "sequence Done p = p"
+  | "sequence (Const c p) p' = Const c (sequence p p')"
+  | "sequence (Load v p) p' = Load v (sequence p p')"
+  | "sequence (Apply b p) p' = Apply b (sequence p p')" 
 
-primrec exec :: "('b \<Rightarrow> 'v \<Rightarrow> 'v \<Rightarrow> 'v) \<Rightarrow> ('a,'v,'b) instr_list \<Rightarrow> ('a \<Rightarrow> 'v) \<Rightarrow> 'v list \<Rightarrow> 'v list"
+fun exec :: "('b \<Rightarrow> 'c \<Rightarrow> 'c \<Rightarrow> 'c) \<Rightarrow> ('v \<Rightarrow> 'c) \<Rightarrow>  ('c,'v,'b) program \<Rightarrow> 'c list \<Rightarrow> 'c list"
 where
-  "exec j Instr_nil s vs = vs" |
-  "exec j (Instr_cons i is) s vs =
-    (case i of Const v \<Rightarrow> exec j is s (v#vs)
-             | Load a \<Rightarrow> exec j is s ((s a)#vs)
-             | Apply f \<Rightarrow> exec j is s ((j f (hd vs) (hd(tl vs)))#(tl(tl vs))))"
+  "exec getBinop env Done stack = stack" 
+  | "exec getBinop env (Const c p) stack = exec getBinop env p (c#stack)" 
+  | "exec getBinop env (Load v p) stack = exec getBinop env p ((env v)#stack)"
+  | "exec getBinop env (Apply b p) stack = 
+     exec getBinop env p ((getBinop b (hd stack) (hd(tl stack)))#(tl(tl stack)))"
 
-
-primrec compile :: "('a,'v,'b) expr \<Rightarrow> ('a,'v,'b) instr_list"
+fun compile :: "('c,'v,'b) expr \<Rightarrow> ('c,'v,'b) program"
   where
-  "compile (Cex v) = Instr_cons (Const v) Instr_nil" |
-  "compile (Vex a) = Instr_cons (Load a) Instr_nil" |
-  "compile (Bex f e1 e2) = instr_app (compile e2) (instr_app (compile e1) (Instr_cons (Apply f) Instr_nil))"
-
+  "compile (Cex c) =  Const c Done" |
+  "compile (Vex v) =  Load v Done" |
+  "compile (Bex b e1 e2) = sequence (compile e2) (sequence (compile e1) (Apply b Done))"
 
 ML{*
-val consts = ["Exp.value", "Exp.exec", "Exp.compile"];
+val consts = ["Exp.value", "Exp.exec", "Exp.compile", "Exp.sequence"];
 
 Hipster_Explore.explore @{context} consts;
+
 *}
+
+lemma lemma_a [thy_expl]: "sequence x4 Done = x4"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms Exp.value.simps Exp.exec.simps Exp.compile.simps Exp.sequence.simps thy_expl} *})
+
+lemma lemma_aa [thy_expl]: "exec x4 y4 (sequence z4 x14) xs4 = exec x4 y4 x14 (exec x4 y4 z4 xs4)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms Exp.value.simps Exp.exec.simps Exp.compile.simps Exp.sequence.simps thy_expl} *})
+
+lemma lemma_ab [thy_expl]: "exec x3 y3 (compile z3) xs3 = value x3 y3 z3 # xs3"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms Exp.value.simps Exp.exec.simps Exp.compile.simps Exp.sequence.simps thy_expl} *})
+
+theorem our_thm : "exec getBinop env (compile e) [] = [value getBinop env e]"
+sledgehammer
+by (metis lemma_ab)
+
+(*
+(* First try by user, gets stuck, call theory exploration, get suitable lemmas. After can prove goal
+by Sledgehammering *)
+apply (induct e)
+apply (simp_all)
+apply (tactic {*Hipster_Explore.explore_goal @{context} consts*})
+ 
+*)
+
 
 end
