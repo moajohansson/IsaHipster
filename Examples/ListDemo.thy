8190b10cd435c50d2cfdc187a706400e3f08e594
moajohansson <moa.johansson@chalmers.se>
2014-03-14 14:01:34 +0100

    faff example for introduction

--- Examples/ListDemo.thy
+++ Examples/ListDemo.thy
@@ -26,6 +26,43 @@ fun qrev :: "'a Lst \<Rightarrow> 'a Lst \<Rightarrow> 'a Lst"
 where 
   "qrev Emp a = a"
 | "qrev (Cons x xs) a = qrev xs (Cons x a)"
+
+lemma lemma_a [thy_expl]: "app x2 Emp = x2"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.rev.simps ListDemo.app.simps thy_expl} *})
+
+lemma lemma_aa [thy_expl]: "app (app x2 y2) z2 = app x2 (app y2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.rev.simps ListDemo.app.simps thy_expl} *})
+
+lemma lemma_ab [thy_expl]: "app (ListDemo.rev x5) (ListDemo.rev y5) = ListDemo.rev (app y5 x5)"
+apply (induct y5)
+sledgehammer
+apply (metis ListDemo.rev.simps(1) app.simps(1) lemma_a)
+sledgehammer
+by (metis ListDemo.rev.simps(2) app.simps(2) lemma_aa)
+
+
+theorem rev_rev : "rev(rev xs) = xs "
+apply (induct xs)
+apply auto
+(* apply (tactic {* Hipster_Explore.explore_goal @{context} ["ListDemo.rev", "ListDemo.app"] *}) *)
+sledgehammer
+by (metis ListDemo.rev.simps(1) ListDemo.rev.simps(2) app.simps(1) app.simps(2) lemma_ab)
+
+(*
+Try first proving lemmas:
+
+lemma lemma_a [thy_expl]: "app x2 Emp = x2"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.rev.simps ListDemo.app.simps thy_expl} *})
+
+lemma lemma_aa [thy_expl]: "app (app x2 y2) z2 = app x2 (app y2 z2)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.rev.simps ListDemo.app.simps thy_expl} *})
+
+lemma lemma_ab [thy_expl]: "app (ListDemo.rev x5) (ListDemo.rev y5) = ListDemo.rev (app y5 x5)"
+by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms ListDemo.rev.simps ListDemo.app.simps thy_expl} *})
+
+then prove the open goal ListDemo.rev (ListDemo.rev xs) = xs by (tactic {*Simp_Metis_Tacs.routine_tac @{context}*})
+*)
+
 ML{*Hipster_Explore.explore @{context} ["ListDemo.app", "ListDemo.rev", "ListDemo.qrev"];
  *}
 lemma lemma_a [thy_expl]: "app x2 Emp = x2"
