3b016bfca16035840fa744451b679ececc5dce7e
moajohansson <moa.johansson@chalmers.se>
2014-05-19 16:34:27 +0200

    Added nicer Isar command for hipster

--- Examples/TreeDemo.thy
+++ Examples/TreeDemo.thy
@@ -17,8 +17,9 @@ where
   "tmap f (Leaf x) = Leaf (f x)"
 | "tmap f (Node l r) = Node (tmap f l) (tmap f r)" 
 
+
 (* First call to Hipster: Explore tmap and mirror *)
-hipster "TreeDemo.mirror" "TreeDemo.tmap"
+hipster mirror tmap 
 lemma lemma_a [thy_expl]: "mirror (tmap x2 y2) = tmap x2 (mirror y2)"
 by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms TreeDemo.mirror.simps TreeDemo.tmap.simps thy_expl} *})
 
@@ -31,14 +32,15 @@ where
   "flat_tree (Leaf x) = [x]"
 | "flat_tree (Node l r) = (flat_tree l) @ (flat_tree r)"
 
-(* Second call to Hipster: Explore relation to lists *)
-hipster "TreeDemo.flat_tree" "TreeDemo.mirror" "TreeDemo.tmap" "List.rev" "List.map"
 
+(* Second call to Hipster: Explore relation to lists *)
+hipster flat_tree mirror tmap rev map
 lemma lemma_ab [thy_expl]: "flat_tree (tmap x2 y2) = map x2 (flat_tree y2)"
 by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms TreeDemo.flat_tree.simps TreeDemo.mirror.simps TreeDemo.tmap.simps List.rev.simps List.map.simps thy_expl} *})
 
 lemma lemma_ac [thy_expl]: "flat_tree (mirror x2) = rev (flat_tree x2)"
 by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms TreeDemo.flat_tree.simps TreeDemo.mirror.simps TreeDemo.tmap.simps List.rev.simps List.map.simps thy_expl} *})
+
 
 fun rigthmost :: "'a Tree \<Rightarrow> 'a"
 where 
@@ -48,7 +50,7 @@ where
 fun leftmost :: "'a Tree \<Rightarrow> 'a"
 where 
   "leftmost (Leaf x) = x"
-|  "leftmost (Node l r) = leftmost l"
+| "leftmost (Node l r) = leftmost l"
 
 (* If we store the tactics in a Proof_Data, then we need to explicitly pass a context around *)
 ML {*val myctxt = Tactic_Data.set_induct_simp @{context} *}
@@ -56,7 +58,7 @@ ML {*val myctxt = Tactic_Data.set_induct_simp @{context} *}
 ML {*Tactic_Data.hard_tac_str myctxt []; *}
 
 (* Third call to Hipster: Rightmost and Leftmost element of a tree. *)
-hipster "List.hd" "List.rev" "TreeDemo.mirror" "TreeDemo.flat_tree" "TreeDemo.rigthmost" "TreeDemo.leftmost"
+hipster hd rev mirror flat_tree rigthmost leftmost
 lemma lemma_ad [thy_expl]: "rigthmost (mirror x2) = leftmost x2"
 by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms List.hd.simps List.rev.simps TreeDemo.mirror.simps TreeDemo.flat_tree.simps TreeDemo.rigthmost.simps TreeDemo.leftmost.simps thy_expl} *})
 
@@ -81,7 +83,7 @@ where
 ML{* Hipster_Explore.explore  @{context} ["TreeDemo.size", "TreeDemo.size1"] *}
 *)
 
-hipster "List.last" "List.rev" "TreeDemo.mirror""TreeDemo.flat_tree" 
+hipster last rev mirror flat_tree 
                                           "TreeDemo.rigthmost" "TreeDemo.leftmost"
 lemma lemma_af [thy_expl]: "last (xs2 @ xs2) = last xs2"
 by (tactic {* Hipster_Tacs.induct_simp_metis @{context} @{thms List.last.simps List.rev.simps TreeDemo.mirror.simps TreeDemo.flat_tree.simps TreeDemo.rigthmost.simps TreeDemo.leftmost.simps thy_expl} *})
--- IsaHipster.thy
+++ IsaHipster.thy
@@ -1,6 +1,7 @@
 theory IsaHipster
 imports Main
 keywords "hipster" :: thy_decl
+
 begin
 
  
@@ -40,19 +41,9 @@ ML_file "HipsterTacs.ML"
 ML_file "ThyExplTacs.ML"
 ML_file "TacticData.ML"
 ML_file "HipsterExplore.ML"
+ML_file "HipsterIsar.ML"
 
-ML{*
-(* Install a Parser for HipSpec. No idea what I'm doing... *)
-let
-fun call_hipster consts = 
-  Local_Theory.target 
-  (fn ctxt => fst(Hipster_Explore.explore ctxt consts));
-in
-Outer_Syntax.local_theory  @{command_spec "hipster"} 
-      "Theory Exploration with Hipster"
-      (Scan.repeat1 Parse.string >> call_hipster)
-      (* (Scan.repeat1 Parse.const >> call_hipster) *)
-end *}
+
 (*
 ML_file "ProofTools.ML"
 ML_file "HipSpec.ML"
