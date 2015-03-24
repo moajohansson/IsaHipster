67681d56d939c70494df66b6fba0fcae546ec8bc
Irene Lobo Valbuena <airinilv@gmail.com>
2015-03-24 18:12:03 +0100

    For now working and context passed on from outside. Will stay for now this way (forget inferred_param)

--- TestTheories/List-prfs/MediumL.thy
+++ TestTheories/List-prfs/MediumL.thy
@@ -162,9 +162,9 @@ by hipster_induct_simp_metis
 
 
 
+(*hipster rotate*)
 
-
-hipster rotate len
+(*hipster rotate len*)
 
 value "(3,4)"
 datatype 'a bitup = Bt 'a 'a
@@ -173,7 +173,7 @@ datatype 'a ptree = Leaf 'a | Node "(('a bitup) ptree)"
 datatype 'a nest = NilN | ConsN "('a \<times> ('a bitup) nest)"
 datatype 'a bush = Ro | Bu "(('a bush) bush)"
 
-hipster rotate app
+(*hipster rotate app*)
 
 thm rotate.induct
 lemma rotSelf : "rotate n (app xs xs) = app (rotate n xs) (rotate n xs)"
--- TestTheories/Naturals.thy
+++ TestTheories/Naturals.thy
@@ -70,6 +70,8 @@ fun lezp :: "Nat \<Rightarrow> bool" where
 fun lezpp :: "Nat \<Rightarrow> bool" where
   "lezpp x = eqN x Z"
 
+
+
 hipster leq lez
 (* TEST: hipster leq lez *)
 (* TEST: hipster_cond lezp leq *)
