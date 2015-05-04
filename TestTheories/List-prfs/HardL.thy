1bb3617794290dfec90899bd096f005d34454e0c
Irene Lobo Valbuena <airinilv@gmail.com>
2015-05-04 14:20:08 +0200

    before pull

--- TestTheories/List-prfs/HardL.thy
+++ TestTheories/List-prfs/HardL.thy
@@ -82,7 +82,6 @@ by (hipster_induct_simp_metis insLen)
 (* XXX: DO NOT RUN with hipster_induct_schemes *)
 lemma appZips  : "len a = len b \<Longrightarrow> app (zip a b) (zip c d) = zip (app a c) (app b d)"
 (*apply(hipster_induct_schemes app.simps len.simps List.exhaust Nat.distinct)*)
-apply(hipster_induct_schemes app.simps len.simps List.exhaust Nat.distinct)
 by (tactic {* let 
       val lemmas = [] (* (ThyExpl_Data.proved_of_ctxt @{context}) @ (Hipster_Rules.get @{context})*)
       val facts = @{thms app.simps  len.simps List.exhaust Nat.distinct} in
