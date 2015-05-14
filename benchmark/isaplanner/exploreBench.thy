b97fc7e7dc452b304dd9243da883f7e7a50bd171
Irene Lobo Valbuena <airinilv@gmail.com>
2015-05-14 14:28:43 +0200

    explore the benchmark

---
+++ benchmark/isaplanner/exploreBench.thy
@@ -0,0 +1,17 @@
+theory exploreBench
+imports Main
+        benchNat
+        benchList
+        benchTree
+        "../../IsaHipster"
+
+begin
+
+hipster plus minus equal2 le lt max2 min2
+
+(*hipster len append elem last butlast butlastConcat rev count delete drop take dropWhile takeWhile map2 filter ins ins1 insort sort zip zipConcat null lastOfTwo sorted
+
+hipster mirror height*)
+
+end
+
