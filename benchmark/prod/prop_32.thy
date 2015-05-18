4265f716abc974f8dd562621592e8a2bc42436a5
Irene Lobo Valbuena <airinilv@gmail.com>
2015-05-18 10:09:50 +0200

    applicant presentation

--- benchmark/prod/prop_32.thy
+++ benchmark/prod/prop_32.thy
@@ -16,7 +16,55 @@ begin
   | "rotate (S z) (Cons2 x2 x3) =
        rotate z (append x3 (Cons2 x2 (Nil2)))"
   (*hipster length append rotate *)
+hipster length append rotate
+lemma lemma_a [thy_expl]: "prop_32.append x2 Nil2 = x2"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma lemma_aa [thy_expl]: "prop_32.rotate x3 Nil2 = Nil2"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma lemma_ab [thy_expl]: "prop_32.append (prop_32.append x2 y2) z2 =
+prop_32.append x2 (prop_32.append y2 z2)"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma lemma_ac [thy_expl]: "prop_32.rotate x2 (Cons2 y2 Nil2) = Cons2 y2 Nil2"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma lemma_ad [thy_expl]: "prop_32.append (prop_32.rotate x3 y3) (prop_32.rotate x3 y3) =
+prop_32.rotate x3 (prop_32.append y3 y3)"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma lemma_ae [thy_expl]: "prop_32.rotate (S Z) (prop_32.rotate x3 y3) = prop_32.rotate (S x3) y3"
+by (hipster_induct_schemes prop_32.length.simps prop_32.append.simps prop_32.rotate.simps)
+
+lemma unknown [thy_expl]: "prop_32.rotate x (prop_32.rotate y z) =
+prop_32.rotate y (prop_32.rotate x z)"
+oops
+
+lemma unknown [thy_expl]: "prop_32.length (prop_32.append x y) = prop_32.length (prop_32.append y x)"
+oops
+
+lemma unknown [thy_expl]: "prop_32.length (prop_32.rotate x y) = prop_32.length y"
+oops
+
+lemma unknown [thy_expl]: "prop_32.rotate (prop_32.length x) x = x"
+oops
+
+lemma unknown [thy_expl]: "prop_32.rotate (S x) (prop_32.rotate y z) =
+prop_32.rotate (S y) (prop_32.rotate x z)"
+oops
+
+lemma unknown [thy_expl]: "prop_32.rotate (prop_32.length x) (prop_32.append x y) = prop_32.append y x"
+oops
+
+lemma unknown [thy_expl]: "prop_32.rotate (prop_32.length x) (prop_32.rotate y x) = prop_32.rotate y x"
+oops
+
+lemma unknown [thy_expl]: "prop_32.rotate (prop_32.length x) (prop_32.append x x) = prop_32.append x x"
+oops
+
   theorem x0 :
     "(rotate (length x) x) = x"
+    by hipster_induct_schemes
     by (tactic {* Subgoal.FOCUS_PARAMS (K (Tactic_Data.hard_tac @{context})) @{context} 1 *})
 end
