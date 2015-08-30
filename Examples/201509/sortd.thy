ef08f7cc5cea0c368ff221480e5f4225bb1b3c71
Irene Lobo Valbuena <airinilv@gmail.com>
2015-08-30 16:48:41 +0200

    Re-organising insertion sort example

--- Examples/201509/sortd.thy
+++ Examples/201509/sortd.thy
@@ -38,7 +38,62 @@ by (hipster_induct_schemes le.simps Nat.exhaust)
 lemma lemma_ab [thy_expl]: "le (S x2) x2 = False"
 by (hipster_induct_schemes le.simps Nat.exhaust)
 
+
+  fun notle :: "Nat => Nat => bool" where
+    "notle x y = (\<not> le x y)"
+
+hipster_cond notle le
+lemma lemma_ac [thy_expl]: "\<not> le (S x12) y12 = le y12 x12"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ad [thy_expl]: "\<not> le y11 x11 \<Longrightarrow> \<not> le x11 y11 = False"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ae [thy_expl]: "\<not> le z11 x11 \<and> \<not> le y11 z11 \<Longrightarrow> le x11 y11 = True"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_af [thy_expl]: "\<not> le z11 y11 \<and> \<not> le x11 z11 \<Longrightarrow> le x11 (S y11) = False"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ag [thy_expl]: "\<not> le z11 y11 \<and> \<not> le x11 z11 \<Longrightarrow>
+\<not> le (S x11) (S y11) = True"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ah [thy_expl]: "\<not> le z11 x11 \<and> \<not> le y11 z11 \<Longrightarrow>
+le (S x11) (S y11) = True"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ai [thy_expl]: "\<not> le z11 y11 \<and> \<not> le x11 z11 \<Longrightarrow>
+le (S x11) (S y11) = False"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_aj [thy_expl]: "\<not> le z11 x11 \<and> \<not> le y11 z11 \<Longrightarrow> \<not> le x11 y11 = False"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_ak [thy_expl]: "\<not> le z11 y11 \<and> \<not> le x11 z11 \<Longrightarrow> \<not> le x11 y11 = True"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
+lemma lemma_al [thy_expl]: "\<not> le z11 y11 \<and> \<not> le x11 z11 \<Longrightarrow> le x11 y11 = False"
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
+
 hipster_cond le
+lemma lemma_am [thy_expl]: "le x2 y2 \<Longrightarrow> le x2 (S y2) = True"
+by (hipster_induct_schemes le.simps Nat.exhaust)
+
+lemma lemma_an [thy_expl]: "le y9 x9 \<and> le x9 y9 \<Longrightarrow> x9 = y9"
+by (hipster_induct_schemes le.simps Nat.exhaust)
+(*
+lemma unknown [thy_expl]: "le z y \<and> le x z \<Longrightarrow> le x y = True"
+oops
+
+lemma unknown [thy_expl]: "le z y \<and> le x z \<Longrightarrow> le x (S y) = True"
+oops
+
+lemma unknown [thy_expl]: "le z x \<and> le y z \<Longrightarrow> le (S x) y = False"
+oops
+
+lemma unknown [thy_expl]: "le z y \<and> le x z \<Longrightarrow> le (S x) (S y) = True"
+oops
 lemma lemma_ac [thy_expl]: "le x2 y2 \<Longrightarrow> le x2 (S y2) = True"
 by (hipster_induct_schemes le.simps Nat.exhaust)
 
@@ -47,12 +102,48 @@ by (hipster_induct_schemes le.simps Nat.exhaust)
 
 lemma lemma_ae [thy_expl]: "le y x \<and> le x y \<Longrightarrow> x = y"
 by (hipster_induct_schemes le.simps Nat.exhaust)
-
+*)
 lemma le_trans [thy_expl]: "le z y \<and> le x z \<Longrightarrow> le x y = True"
 by (hipster_induct_schemes le.simps Nat.exhaust)
 
 
-(*hipster_cond sorted isort insert le *)
+hipster_cond sorted isort insert le
+lemma lemma_ao [thy_expl]: "insert Z (insert x22 y22) =
+insert x22 (insert Z y22)"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma lemma_ap [thy_expl]: "sorted (insert Z x4) = sorted x4"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma lemma_aq [thy_expl]: "insert Z (isort x3) = isort (insert Z x3)"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma lemma_ar [thy_expl]: "insert x (insert y z) = insert y (insert x z)"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma lemma_as [thy_expl]: "isort (insert x y) = insert x (isort y)"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma unknown []: "sorted (insert x y) = sorted y"
+oops
+
+lemma lemma_at [thy_expl]: "isort (isort x) = isort x"
+by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps list.exhaust Nat.exhaust)
+
+lemma unknown []: "sorted (isort x) = True"
+oops
+
+lemma unknown [thy_expl]: "sorted x \<Longrightarrow> isort x = x"
+oops
+
+lemma unknown [thy_expl]: "sorted y \<Longrightarrow> isort (insert x y) = insert x y"
+oops
+
+lemma unknown [thy_expl]: "sorted y \<Longrightarrow> sorted (insert x y) = True"
+oops
+
+lemma unknown [thy_expl]: "sorted x \<Longrightarrow> isort (insert Z x) = insert Z x"
+oops
 lemma lemma_af [thy_expl]: "insert Z (insert x20 y20) = insert x20 (insert Z y20)"
 by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps)
 
@@ -61,25 +152,23 @@ by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps)
 
 lemma lemma_ah [thy_expl]: "insert Z (isort x3) = isort (insert Z x3)"
 by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps)
+*)
 
-  fun notle :: "Nat => Nat => bool" where
-    "notle x y = (\<not> le x y)"
-
-hipster_cond notle
+(*hipster_cond notle
 hipster notle le
 lemma lemma_adi [thy_expl]: "\<not> le (S x2) y2 = le y2 x2"
-by (hipster_induct_schemes sortd.notle.simps sortd.Nat.exhaust)
+by (hipster_induct_schemes notle.simps Nat.exhaust)
 
 lemma lemma_adj [thy_expl]: "\<not> le x2 y2 \<Longrightarrow> \<not> le x2 Z = True"
-by (hipster_induct_schemes sortd.notle.simps sortd.Nat.exhaust)
+by (hipster_induct_schemes notle.simps Nat.exhaust)
 
 lemma lemma_ [thy_expl]: "le (S x11) y11 = (\<not> le y11 x11)"
-by (hipster_induct_schemes sortd.notle.simps sortd.le.simps sortd.Nat.exhaust)
+by (hipster_induct_schemes notle.simps le.simps Nat.exhaust)
 
 (* include negation: discovered if declared separately *)
 lemma nole []: "\<not> le r s \<Longrightarrow> le s r"
 by (hipster_induct_schemes le.simps Nat.exhaust)
-
+*)
 (*hipster_cond sorted isort insert le*)
 lemma lemma_ai [thy_expl]: "insert x32 (insert y32 z32) = insert y32 (insert x32 z32)"
 by (hipster_induct_schemes sorted.simps isort.simps insert.simps le.simps)
