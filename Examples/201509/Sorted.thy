fe93ff5962ea87671d600bc527f1a5e5f23c6263
Irene Lobo Valbuena <airinilv@gmail.com>
2015-09-01 20:30:42 +0200

    Demo all set

--- Examples/201509/Sorted.thy
+++ Examples/201509/Sorted.thy
@@ -18,14 +18,14 @@ begin
   | "sorted (Cons y (Cons y2 ys)) =
        (if le y y2 then sorted (Cons y2 ys) else False)"
 
-  fun insort :: "Nat => Nat list => Nat list" where
-    "insort x Nil = Cons x Nil"
-  | "insort x (Cons z xs) =
-       (if le x z then Cons x (Cons z xs) else Cons z (insort x xs))"
+  fun insert :: "Nat => Nat list => Nat list" where
+    "insert x Nil = Cons x Nil"
+  | "insert x (Cons z xs) =
+       (if le x z then Cons x (Cons z xs) else Cons z (insert x xs))"
 
   fun sort :: "Nat list => Nat list" where
   "sort Nil = Nil"
-  | "sort (Cons y xs) = insort y (sort xs)"
+  | "sort (Cons y xs) = insert y (sort xs)"
 
 (* First: explore the most simple function, ie: that which is defined in terms of itself alone *)
 (*hipster le*)
@@ -54,64 +54,62 @@ by (hipster_induct_schemes le.simps Nat.exhaust)
 
 (* We proceed on to explore other functions in the theory, and we find some properties we
     cannot prove yet *)
-(*hipster sorted insort le*)
-lemma lemma_ag [thy_expl]: "insort Z (insort x19 y19) = insort x19 (insort Z y19)"
-by (hipster_induct_schemes sorted.simps insort.simps le.simps list.exhaust Nat.exhaust)
+(*hipster sorted insert le*)
+lemma lemma_ag [thy_expl]: "insert Z (insert x19 y19) = insert x19 (insert Z y19)"
+by (hipster_induct_schemes sorted.simps insert.simps le.simps list.exhaust Nat.exhaust)
 
-lemma lemma_ah [thy_expl]: "sorted (insort Z x4) = sorted x4"
-by (hipster_induct_schemes sorted.simps insort.simps le.simps list.exhaust Nat.exhaust)
+lemma lemma_ah [thy_expl]: "sorted (insert Z x4) = sorted x4"
+by (hipster_induct_schemes sorted.simps insert.simps le.simps list.exhaust Nat.exhaust)
 
-lemma unknown [thy_expl]: "insort x (insort y z) = insort y (insort x z)"
+lemma unknown [thy_expl]: "insert x (insert y z) = insert y (insert x z)"
 oops
 
-lemma unknown [thy_expl]: "sorted (insort x y) = sorted y"
+lemma unknown [thy_expl]: "sorted (insert x y) = sorted y"
 oops
 
 (* Neither when considering conditionals with the predicate _sorted_ *)
-(*hipster_cond sorted insort le *)
-lemma unknown [thy_expl]: "insort x (insort y z) = insort y (insort x z)"
+(*hipster_cond sorted insert le *)
+lemma unknown [thy_expl]: "insert x (insert y z) = insert y (insert x z)"
 oops
 
-lemma unknown [thy_expl]: "sorted (insort x y) = sorted y"
+lemma unknown [thy_expl]: "sorted (insert x y) = sorted y"
 oops
 
-lemma unknown [thy_expl]: "sorted y \<Longrightarrow> sorted (insort x y) = True"
+lemma unknown [thy_expl]: "sorted y \<Longrightarrow> sorted (insert x y) = True"
 oops
 
 
 (* So we might think about exploring the negation of some predicate, namely of that which
-    defines branching in our sorting funcitons. Right now, we need to define it separately
+    defines branching in our sorting functions. Right now, we need to define it separately
     for exploration purposes *)
-fun nole:: "Nat \<Rightarrow> Nat \<Rightarrow> bool" where
-  "nole x y = (\<not> le x y)"
+fun notle:: "Nat \<Rightarrow> Nat \<Rightarrow> bool" where
+  "notle x y = (\<not> le x y)"
 
 (* And we finally explore conditional properties for it *)
-(*hipster_cond nole*)
-lemma lemma_ai [thy_expl]: "nole (S x2) y2 = le y2 x2"
-by (hipster_induct_schemes  Nat.exhaust)
+(*hipster_cond notle*)
+lemma lemma_ai [thy_expl]: "notle (S x2) y2 = le y2 x2"
+by (hipster_induct_schemes notle.simps Nat.exhaust)
 
-lemma lemma_aj [thy_expl]: "nole x2 y2 \<Longrightarrow> nole x2 Z = True"
-by (hipster_induct_schemes  Nat.exhaust)
+lemma lemma_aj [thy_expl]: "notle x2 y2 \<Longrightarrow> notle x2 Z = True"
+by (hipster_induct_schemes notle.simps Nat.exhaust)
 
 
-(* If we now revisit one of the lemmas discovered about _sorted_ and _insort_, we will be able
+(* If we now revisit one of the lemmas discovered about _sorted_ and _insert_, we will be able
     to prove after prior modification of the options: using full_types in metis and increasing
     the timeout for proof search (namely that for metis) *)
 setup{* Hip_Tac_Ops.toggle_full_types @{context} ;*}
-setup{* Hip_Tac_Ops.set_metis_to @{context} 5000;*}
-hipster_cond sorted insort le nole
-lemma lemma_ak [thy_expl]: "Sorted.insort x30 (Sorted.insort y30 z30) =
-Sorted.insort y30 (Sorted.insort x30 z30)"
-by (hipster_induct_schemes Sorted.sorted.simps Sorted.insort.simps Sorted.le.simps Sorted.list.exhaust Sorted.Nat.exhaust)
+setup{* Hip_Tac_Ops.set_metis_to @{context} 3500;*}
 
-lemma lemma_al [thy_expl]: "Sorted.sorted y31 \<Longrightarrow>
-Sorted.sorted (Sorted.insort x31 y31) = True"
-by (hipster_induct_schemes Sorted.sorted.simps Sorted.insort.simps Sorted.le.simps Sorted.list.exhaust Sorted.Nat.exhaust)
+(*hipster_cond sorted (*sort*) insert le notle*)
+lemma lemma_ak [thy_expl]: "insert x30 (insert y30 z30) = insert y30 (insert x30 z30)"
+by (hipster_induct_schemes sorted.simps insert.simps le.simps notle.simps list.exhaust Nat.exhaust)
 
-lemma unknown [thy_expl]: "Sorted.sorted (Sorted.insort x y) = Sorted.sorted y"
+lemma lemma_al [thy_expl]: "sorted y31 \<Longrightarrow> sorted (insert x31 y31) = True"
+by (hipster_induct_schemes sorted.simps insert.simps le.simps notle.simps list.exhaust Nat.exhaust)
+
+lemma unknown [thy_expl]: "sorted (insert x y) = sorted y"
 oops
-lemma lemmaak []: "sorted y \<Longrightarrow> sorted (insort x y) = True"
-by (hipster_induct_schemes sorted.simps nole.simps insort.simps le.simps Nat.exhaust list.exhaust)
+
 
 (* We can finally immediately prove our target theorem! *)
   theorem x0 :
