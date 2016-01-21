62c8193c84d7e4c1afbb57e17a86903ee85d9f2d
Irene Lobo Valbuena <airinilv@gmail.com>
2016-01-21 16:03:07 +0100

    Error printing messages. Bug still on the loose.

--- bugged/GeneralisedErr.thy
+++ bugged/GeneralisedErr.thy
@@ -21,6 +21,17 @@ hipster rotate
 
 lemma problem : "rotate (S Z) (rotate x y) = rotate (S x) y"
 by hipster_induct_schemes
+
+lemma generalP: "rotate x (rotate y z) = rotate y (rotate x z)"
+apply(induction y z rule: rotate.induct)
+apply simp_all
+sledgehammer
+apply (smt Nat.inject problem rotate.elims rotate.simps(1) rotate.simps(2))
+
+by (hipster_induct_schemes rotate.simps Nat.exhaust problem)
+
+oops
+
 (* Actually equivalent to:
     apply(induction x y rule: rotate.induct)
     apply simp_all *)
