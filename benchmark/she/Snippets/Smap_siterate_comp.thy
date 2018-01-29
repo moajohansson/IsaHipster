fd7a224e24655d012d3b3cc0150fac7a03c893da
solrun <solalola@gmail.com>
2018-01-29 18:41:56 +0100

    more snippets

---
+++ benchmark/she/Snippets/Smap_siterate_comp.thy
@@ -0,0 +1,25 @@
+theory Smap_siterate_comp
+  imports Main "$HIPSTER_HOME/IsaHipster"
+    Smap_laws
+begin
+setup Tactic_Data.set_coinduct_sledgehammer
+primcorec siterate :: "('a \<Rightarrow> 'a) \<Rightarrow> 'a \<Rightarrow> 'a Stream" where
+  "siterate f a = SCons a (siterate f (f a))"
+cohipster smap siterate
+lemma lemma_ac [thy_expl]: "smap y (siterate y z) = siterate y (y z)"
+  by (coinduction arbitrary: y z rule: Stream.Stream.coinduct_strong)
+    auto
+
+lemma lemma_ad [thy_expl]: "smap z (SCons y (siterate z x2)) = SCons (z y) (siterate z (z x2))"
+ by (coinduction arbitrary: x2 y z rule: Stream.Stream.coinduct_strong)
+    (simp add: lemma_ac)
+text_raw {*\DefineSnippet{siteratefusion}{*}
+cohipster smap siterate Fun.comp \<comment> "Explore smap, siterate, and function composition"
+
+lemma lemma_ae [thy_expl]: "smap z (siterate (y \<circ> z) x2) = siterate (z \<circ> y) (z x2)"
+ by (coinduction arbitrary: x2 y z rule: Stream.Stream.coinduct_strong)
+    auto
+text_raw {*}%EndSnippet*}
+      
+
+end
